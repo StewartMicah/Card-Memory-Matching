2823b4fd7bca1d5d9b72b8f3843ea1581f93c1e3
Beepo <micahstew@gmail.com>
2022-06-24 20:05:01 -0400

    adding files

---
+++ react-tutorial/src/index.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+
+const cardStyle = {
+  
+}
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello, react!</h1>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
